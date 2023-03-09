3d0013e790242b9cb3eb2e120e2592d4641b8612
dgeorgiev <dgeorgiev@web3tech.ru>
2023-03-09 15:12:43 +0000

    [WTCH-145] Add core modules of tx-observer

--- we-tx-observer-jpa-specification-utils/build.gradle.kts
+++ we-tx-observer-jpa-specification-utils/build.gradle.kts
@@ -1,14 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 
 dependencies {
     implementation(kotlin("stdlib"))
 
     implementation("jakarta.persistence:jakarta.persistence-api")
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
 }
