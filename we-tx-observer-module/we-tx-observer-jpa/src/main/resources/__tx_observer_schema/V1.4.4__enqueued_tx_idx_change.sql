e53f0fda13275c4bac9049dc5fcc2220de0fa062
ashalaginov <ashalaginov@web3tech.ru>
2024-01-12 13:14:19 +0300

    [WTCH-204] Change index on enqueued_tx.partition_id

--- we-tx-observer-module/we-tx-observer-jpa/src/main/resources/__tx_observer_schema/V1.4.4__enqueued_tx_idx_change.sql
+++ we-tx-observer-module/we-tx-observer-jpa/src/main/resources/__tx_observer_schema/V1.4.4__enqueued_tx_idx_change.sql
@@ -1,1 +1,5 @@
 drop index if exists ix__enqueued_tx_partition_id_timestamp_for_new;
+
+create index if not exists ix__enqueued_tx_partition_id_for_new
+    on enqueued_tx(partition_id)
+    where (status = 'NEW');
